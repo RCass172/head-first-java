adb8c3b2016a0156bc26f1682d3d50f5024bbf79
Ruth Cassidy <ruth.cassidy@zinkworks.com>
2022-04-20 16:35:02 +0100

    Rename class to java standards

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,5 @@
 
-public class hello {
+public class Hello {
     public static void main(String[] args) {
         System.out.println("And so it begins..");
         // Calls and runs the while loop method
