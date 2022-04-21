c6a17ae926ed642d21507fd0ccebd151005e5aa3
Ruth Cassidy <ruth.cassidy@zinkworks.com>
2022-04-21 13:56:21 +0100

    Add two new exercises, puzzles one and shuffle

--- src/Hello.java
+++ src/Hello.java
@@ -7,6 +7,8 @@ public class Hello {
         dooBee();
         System.out.println("");
         greenBottles();
+        shuffle1();
+        poolPuzzleOne();
     }
 
     // Creates the intro method
@@ -53,6 +55,47 @@ public class Hello {
             } else {
                 System.out.println("There'll be no green bottles of beer on the wall :'(");
             }
+        }
+    }
+
+    public static void shuffle1() {
+        int x = 3;
+        while (x > 0) {
+            if (x > 2) {
+                System.out.print("a");
+            }
+            x--;
+            System.out.print("-");
+            if (x == 2) {
+                System.out.print("b c");
+            }
+            if (x == 1) {
+                System.out.println("d");
+                x--;
+            }
+        }
+    }
+
+    public static void poolPuzzleOne() {
+        int x = 0;
+        while(x < 4) {
+            System.out.print("a");
+            if (x < 1) {
+                System.out.print(" ");
+            }
+            System.out.print("n");
+            if (x > 1) {
+                System.out.print(" oyster");
+                x = x + 2;
+            }
+            if (x == 1) {
+                System.out.print("noys");
+            }
+            if (x < 1) {
+                System.out.print("oise");
+            }
+            System.out.println();
+            x++;
         }
     }
 }
