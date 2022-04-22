313db4b195731205010d39dfd6573803682f3d83
Ruth Cassidy <ruth.cassidy@zinkworks.com>
2022-04-22 12:27:40 +0100

    Add the echo class and method to complete exercise

--- src/Hello.java
+++ src/Hello.java
@@ -11,6 +11,7 @@ public class Hello {
         poolPuzzleOne();
         dog();
         cat();
+        echoTestDrive();
     }
 
     // Creates the intro method
@@ -123,5 +124,23 @@ public class Hello {
         System.out.println("I'm " + animal2.name + " the " + animal2.type);
 
         animal2.speak(5);
+    }
+
+    public static void echoTestDrive() {
+        Echo e1 = new Echo();
+        Echo e2 = new Echo();
+        int x = 0;
+        while ( x < 4 ) {
+            e1.hello();
+            e1.count = e1.count + 1;
+            if ( x == 3 ) {
+                e2.count = e2.count + 1;
+            }
+            if ( x > 0 ) {
+                e2.count = e2.count + e1.count;
+            }
+            x = x + 1;
+        }
+        System.out.println(e2.count);
     }
 }
